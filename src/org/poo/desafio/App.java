b71a6119e033399fc117ba90e76174c389c52eac
Alexandre Gelande <agelande@gmail.com>
2023-09-14 15:34:14 -0300

    Implementado métodos da classe Iphone.

--- src/org/poo/desafio/App.java
+++ src/org/poo/desafio/App.java
@@ -2,6 +2,8 @@ package org.poo.desafio;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello wourld!");
+
+        Iphone iphone = new Iphone();
+
     }
 }
--- src/org/poo/desafio/Iphone.java
+++ src/org/poo/desafio/Iphone.java
@@ -1,4 +1,76 @@
 package org.poo.desafio;
 
-public class Iphone {
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+
+    private int numeroMusica;
+    private long numeroTelefone;
+
+    public int getNumeroMusica() {
+        return numeroMusica;
+    }
+
+    public void setNumeroMusica(int numeroMusica) {
+        this.numeroMusica = numeroMusica;
+    }
+
+    public long getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(long numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    @Override
+    public void ligar(long numeroTelefone) {
+        System.out.println("Ligando para o número " + numeroTelefone);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atender.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página.");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página.");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música em pausa.");
+    }
+
+    @Override
+    public void selecionarMusica(int numeroMusica) {
+        System.out.println("Música " + numeroMusica + " selecionada." );
+    }
+
+    @Override
+    public String toString() {
+        return "Iphone{" +
+                "numeroMusica=" + numeroMusica +
+                ", numeroTelefone=" + numeroTelefone +
+                '}';
+    }
 }
